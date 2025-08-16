4012a15458122703b3dbc8d3b135b85567a5cf87
DharshanSR <dharshanravindran8@gmail.com>
2025-08-16 21:46:06 +0530

    feat: add descriptive text to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Dharshan</div>
+    <div>Dharshan
+      <div>
+        <p>leuhqpnfufqbyf8qf 9qnfuef
+          qpe fpe uf weufpuwe9fnwpgfug0ugprrty0p4uyuuq4p yuq54[ n9045 ni   tn80 3y [tp   utpoq 
+          g n-qitp  utpoq3]]
+        </p>
+      </div>
+    </div>
   );
 }
